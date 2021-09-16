32dbe60691ef222a8faba1ea5ff651d236f2632f
r-ybarra <83841580+r-ybarra@users.noreply.github.com>
2021-09-16 13:03:34 -0500

    updated logic

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -40,6 +40,7 @@ let cities = [{
 // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
     console.log(city)
+    L.marker(city.location).addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.
